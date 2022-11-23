3d21498e89b6a19931cf5e61bab160da74f57f11
Ogunneye oreoluwa <88655756+orizee01@users.noreply.github.com>
2022-11-23 13:47:40 +0100

    modified port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config()
 const todoRouter = require('./src/routes/user_route')
-const port = process.env.PORT
+const port = process.env.PORT || 5000;
 
 
 const app = express();
