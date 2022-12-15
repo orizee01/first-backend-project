cc9fa7aa14ceb78341be0b4a24fed9fef792d9bb
oreoluwa ogunneye <oreoluwa@oreoluwas-MacBook-Pro.local>
2022-12-15 15:09:22 +0100

    minor-changes

--- src/middlewares/todoMiddleware.js
+++ src/middlewares/todoMiddleware.js
@@ -6,49 +6,44 @@ module.exports = {
       const { id } = req.params;
       const todo = await getAlltodo(id);
       console.log(todo);
-        if (todo === null) {
+      if (todo === null) {
         return res.status(403).json({
           status: "fail",
           message: "todo does not exist",
         });
-      } 
+      }
       req.todo = todo;
       next();
     } catch (err) {
-        console.log(err.message, 'chex');
+      console.log(err.message, "chex");
       res.status(400).json({
         status: "fail",
         data: null,
         message: err.message,
-      })
+      });
     }
   },
 
   CheckIfTodoBelongsToUser: async (req, res, next) => {
-   try {
-   
-      
-     const userId = req.todo.users_id
-     console.log(userId, "end todo")
-     const id = req.decoded.id
-   
-     if(userId !== id ) {
-      return res.status(403).json({
-       status: "fail",
-       message: "todo does not Belong to the user",
-      })
-    }
-    
-    next(); 
-  } catch (err) {
-    res.status(400).json({
-        status:"fail",
+    try {
+      const userId = req.todo.users_id;
+      console.log(userId, "end todo");
+      const id = req.decoded.id;
+
+      if (userId !== id) {
+        return res.status(403).json({
+          status: "fail",
+          message: "todo does not Belong to the user",
+        });
+      }
+
+      next();
+    } catch (err) {
+      res.status(400).json({
+        status: "fail",
         data: null,
         message: err.message,
-    })
-
-   }
-  
-
-}
-}
+      });
+    }
+  },
+};
